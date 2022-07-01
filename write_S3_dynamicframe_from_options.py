3377f6f33a16cec75612d8d2d88ca50581aef9f8
Christopher Sharkey <ev2900@gmail.com>
2022-07-01 13:15:21 -0500

    Update write_S3_dynamicframe_from_options.py

---
+++ write_S3_dynamicframe_from_options.py
@@ -0,0 +1,7 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
