cc9d59e22ac60aa5e98c7bdfdb1270ad6588cdaf
Christopher Sharkey <ev2900@gmail.com>
2022-07-02 18:04:09 -0500

    Update write_S3_dynamicframe_from_options.py

--- write_S3_dynamicframe_from_options.py
+++ write_S3_dynamicframe_from_options.py
@@ -5,3 +5,6 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.dynamicframe import DynamicFrame
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
